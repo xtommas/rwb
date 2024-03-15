4f9165c8454036cf97db388eb180163f9b6e4ec3
Tomas <tomascanepa1397@gmail.com>
2024-03-15 12:54:24 -0300

    api request and DTO initial setup

---
+++ api/DTO/response.go
@@ -0,0 +1,36 @@
+package dto
+
+type Response struct {
+	Wallpaper []struct {
+		ID         string   `json:"id"`
+		URL        string   `json:"url"`
+		ShortURL   string   `json:"short_url"`
+		Views      int      `json:"views"`
+		Favorites  int      `json:"favorites"`
+		Source     string   `json:"source"`
+		Purity     string   `json:"purity"`
+		Category   string   `json:"category"`
+		DimensionX int      `json:"dimension_x"`
+		DimensionY int      `json:"dimension_y"`
+		Resolution string   `json:"resolution"`
+		Ratio      string   `json:"ratio"`
+		FileSize   int      `json:"file_size"`
+		FileType   string   `json:"file_type"`
+		CreatedAt  string   `json:"created_at"`
+		Colors     []string `json:"colors"`
+		Path       string   `json:"path"`
+		Thumbs     struct {
+			Large    string `json:"large"`
+			Original string `json:"original"`
+			Small    string `json:"small"`
+		} `json:"thumbs"`
+	} `json:"data"`
+	Meta struct {
+		CurrentPage int    `json:"current_page"`
+		LastPage    int    `json:"last_page"`
+		PerPage     int    `json:"per_page"`
+		Total       int    `json:"total"`
+		Query       string `json:"query"`
+		Seed        string `json:"seed"`
+	} `json:"meta"`
+}
